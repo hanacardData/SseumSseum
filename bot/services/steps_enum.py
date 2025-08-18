4700bb2233f7cb260aadfdf859996dde7c3bc657
unknown <rektpunk@gmail.com>
2025-08-18 16:51:12 +0900

    add steps enum:

---
+++ bot/services/steps_enum.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+
+class Step(str, Enum):
+    START = "start"
+    GENERATE = "generate"
+    CHANNEL = "channel"
+    PURPOSE = "purpose"
+    TARGET = "target"
+    DESCRIPTION = "description"
+
+
+class Start(str, Enum):
+    COPY_GENERATE = "카피 생성하기"
+    COPY_FIX = "카피 다듬기"
+
+
+class Channel(str, Enum):
+    PUSH = "PUSH 메세지"
+    LMS = "LMS"
+    SMS = "SMS"
+    TALK = "알림톡"
+
+
+class Purpose(str, Enum):
+    CARD_ISSUE = "카드 발급 유도"
+    CARD_USE = "카드 사용 유도"
+    CARD_RENEWAL = "카드 갱신 유도"
+    APP_SIGNUP = "앱 가입 유도"
+    APP_USE = "앱 사용 유도"
+    APP_FEATURE_USE = "앱 특정 기능 사용"
+    EVENT_JOIN = "이벤트 응모"
+    SURVEY = "만족도 조사"
+    PRODUCT_RECOMMEND = "특정 상품 권유"
