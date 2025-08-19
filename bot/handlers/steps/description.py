37213cb358d7ea9b2121c0be19a874de8d5e9314
unknown <rektpunk@gmail.com>
2025-08-19 14:02:00 +0900

    update description

--- bot/handlers/steps/description.py
+++ bot/handlers/steps/description.py
@@ -10,6 +10,8 @@ async def handle_description_input_event(
 ) -> None:
     """캠페인 설명 입력 이벤트, step=="purpose" 인 경우 실행
     text는 캠페인 설명
+    선택한 캠페인 설명에 따라 Session에 step="description"로 설정
+    이후 다시 start step으로 돌아가게 함
     """
     context = session["context"]
     context[Step.DESCRIPTION.value] = text
